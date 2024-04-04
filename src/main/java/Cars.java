c996450c4ca88abd74b755d897bec14b4a99ee61
Daniel-k-dev <juyeon10120@naver.com>
2024-04-04 19:26:54 +0900

    feat(Cars): Cars 생성

---
+++ src/main/java/Cars.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> cars = new LinkedList<>();
+    public void add(Car car) {
+        this.cars.add(car);
+    }
+
+    public String generateReport() {
+        StringBuilder report = new StringBuilder();
+
+        for (Car car : this.cars) {
+            report.append(car.generateReport() + "\n");
+            report.append("------------------------------" + "\n");
+        }
+
+        return report.toString();
+    }
+}
