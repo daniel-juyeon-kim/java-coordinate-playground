30db254edfca67da5d4523958ec1d6e63fd33549
Daniel-k-dev <juyeon10120@naver.com>
2024-04-12 22:17:21 +0900

    refactor(Triangle): 메서드의 길이, 파라미터의 개수 제한을 충족하도록 수정

--- src/main/java/calculator/Triangle.java
+++ src/main/java/calculator/Triangle.java
@@ -1,14 +1,11 @@
 package calculator;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
 
 public class Triangle extends Shape{
-
-    private final int POINTS_SIZE = 3;
     private final String RESULT_MESSAGE = "삼각형 넓이는 ";
-    private final String INVALID_POINTS_SIZE = "점이 " + POINTS_SIZE + "개가 아닙니다.";
+    private final String INVALID_POINTS_SIZE = "점이 " + TRIANGLE_POINT_SIZE + "개가 아닙니다.";
 
     public Triangle(String values) {
         super(values);
@@ -27,27 +24,39 @@ public class Triangle extends Shape{
     }
 
     private boolean isThree (int value)  {
-        return POINTS_SIZE == value;
+        return TRIANGLE_POINT_SIZE == value;
     }
 
     @Override
     String calculateArea() {
-        Point point1 = points.get(0);
-        Point point2 = points.get(1);
-        Point point3 = points.get(2);
+        List<Double> lines = createLines();
 
-        double line1 = point1.calculateDistance(point2);
-        double line2 = point2.calculateDistance(point3);
-        double line3 = point3.calculateDistance(point1);
-
-        double s = (line1 + line2 + line3) / 2;
-
-        double area = Math.sqrt(s * (s - line1) * (s - line2) * (s - line3));
+        double area = calculateOfficial(lines);
 
         return Double.toString(area);
     }
 
+    private List<Double> createLines() {
+        Point point1 = points.get(0);
+        Point point2 = points.get(1);
+        Point point3 = points.get(2);
 
+        return Arrays.asList(
+                point1.calculateDistance(point2),
+                point2.calculateDistance(point3),
+                point3.calculateDistance(point1)
+        );
+    }
+
+    private double calculateOfficial(List<Double> lines) {
+        double line1 = lines.get(0);
+        double line2 = lines.get(1);
+        double line3 = lines.get(2);
+
+        double s = (line1 + line2 + line3) / 2;
+
+        return Math.sqrt(s * (s - line1) * (s - line2) * (s - line3));
+    }
 
     @Override
     String generateResultMessage() {
