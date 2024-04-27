4f1bf0bc971d330c7a8cdf86109a7d9f2041b102
Daniel-k-dev <juyeon10120@naver.com>
2024-04-28 01:14:09 +0900

    feat(coordinate): 좌표계산기 다시 구현을 위한 삭제

--- src/main/java/coordinate/CoordinateMain.java
+++
@@ -1,9 +0,0 @@
-package coordinate;
-
-import coordinate.controller.CoordinateController;
-
-public class CoordinateMain {
-    public static void main(String[] args) {
-        CoordinateController.run();
-    }
-}
